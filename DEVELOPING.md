8d312e8ae887e89fd059b4df60e13c8d771f967d
Lindsay Evans <lindsay.evans@boomworks.com.au>
2024-06-22 14:41:06 +1000

    feat: add support for remote templates

--- DEVELOPING.md
+++ DEVELOPING.md
@@ -4,4 +4,6 @@ npm link
 pnpm start examples/simple --name="My Simple Component" --includeStyle=false
 pnpm start examples/react-component --name="My React Component" --includeStyle=false
 pnpm start examples/kitchen-sink --name="My Kitchen Sink" --includeStyle=false
+pnpm start https://raw.githubusercontent.com/lindsayevans/scffld/develop/examples/simple.md --name="My Remote Component"
+pnpm start github:lindsayevans/scffld/examples/simple --name="My Github Component"
 ```
--- src/cli.ts
+++ src/cli.ts
@@ -25,56 +25,94 @@ program
   .allowUnknownOption()
   .allowExcessArguments();
 
-const template = process.argv[2];
-const templateContent = fs.readFileSync(
-  path.resolve(template + (template.endsWith('.md') ? '' : '.md'))
-);
+const main = async () => {
+  const template = process.argv[2];
 
-console.log('');
-const spinner = ora(`Scaffolding template ${template}...`).start();
+  let templateContent = '';
 
-let codeblocks: any[] = [];
-let params: any;
+  if (
+    template.startsWith('http:') ||
+    template.startsWith('https:') ||
+    template.startsWith('github:')
+  ) {
+    console.log('');
+    const fetchSpinner = ora(`Fetching template ${template}...`).start();
+    let url = template;
 
-const md = markdownit();
-const frontmatter = templateContent.toString().split('---')[1];
-const fmParsed = YAML.parse(frontmatter);
-if (fmParsed) {
-  params = { ...fmParsed };
-  if (fmParsed.props) {
-    Object.keys(fmParsed.props).forEach((name: string) => {
-      const option = new Option(`--${name}`);
-      option.required = fmParsed.props[name].required || false;
-      option.optional = !option.required;
-      option.defaultValue = fmParsed.props[name].default;
-      if (fmParsed.props[name].type === 'boolean') {
-        // @ts-expect-error
-        option.parseArg = (value: string) => value === 'true';
-      }
-      program.addOption(option);
-    });
-  }
-}
+    if (template.startsWith('github:')) {
+      const parts = template.replace('github:', '').split('/');
+      url = `https://raw.githubusercontent.com/${parts[0]}/${
+        parts[1]
+      }/HEAD/${parts.slice(2).join('/')}${
+        template.endsWith('.md') ? '' : '.md'
+      }`;
+    }
 
-program.action((template, options) => {
-  if (!params) {
-    params = { options };
+    const response = await fetch(url);
+    templateContent = await response.text();
+    fetchSpinner.prefixText = '✅';
+    fetchSpinner.stopAndPersist();
   } else {
-    params.options = options;
+    templateContent = fs
+      .readFileSync(
+        path.resolve(template + (template.endsWith('.md') ? '' : '.md'))
+      )
+      .toString();
   }
 
-  if (codeblocks && codeblocks.length > 0) {
+  if (templateContent === '') {
+    console.error('No template content :(');
+    process.exit(1);
+  }
+
+  console.log('');
+  const spinner = ora(`Scaffolding template ${template}...`).start();
+
+  let codeblocks: any[] = [];
+  let params: any;
+
+  const md = markdownit();
+  const frontmatter = templateContent.split('---')[1];
+  const fmParsed = YAML.parse(frontmatter);
+  if (fmParsed) {
+    params = { ...fmParsed };
+    if (fmParsed.props) {
+      Object.keys(fmParsed.props).forEach((name: string) => {
+        const option = new Option(`--${name}`);
+        option.required = fmParsed.props[name].required || false;
+        option.optional = !option.required;
+        option.defaultValue = fmParsed.props[name].default;
+        if (fmParsed.props[name].type === 'boolean') {
+          // @ts-expect-error
+          option.parseArg = (value: string) => value === 'true';
+        }
+        program.addOption(option);
+      });
+    }
+  }
+
+  program.action((template, options) => {
+    if (!params) {
+      params = { options };
+    } else {
+      params.options = options;
+    }
+
+    if (codeblocks && codeblocks.length > 0) {
+      renderCodeblocks(codeblocks, params, spinner, startTime);
+    }
+  });
+
+  program.parse(process.argv);
+
+  const parsedTemplate = md.parse(templateContent, {});
+  codeblocks = parsedTemplate.filter(
+    (x) => x.type === 'fence' && x.tag === 'code'
+  );
+
+  if (params && params.options) {
     renderCodeblocks(codeblocks, params, spinner, startTime);
   }
-});
+};
 
-program.parse(process.argv);
-
-const parsedTemplate = md.parse(templateContent.toString(), {});
-codeblocks = parsedTemplate.filter(
-  (x) => x.type === 'fence' && x.tag === 'code'
-);
-
-if (params && params.options) {
-  renderCodeblocks(codeblocks, params, spinner, startTime);
-}
+main();
