670017ac835309a0225cbe7b2905aea31e5f8279
Lindsay Evans <lindsay.evans@boomworks.com.au>
2024-06-27 20:27:27 +1000

    feat(generator): stub out generator

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
+import { GENERATE_COMMAND, generate } from './cli/generate.js';
 import { scaffold } from './cli/scaffold.js';
 
-scaffold(process.argv);
+if (process.argv[2] === GENERATE_COMMAND) {
+  generate(process.argv);
+} else {
+  scaffold(process.argv);
+}
---
+++ src/cli/generate.ts
@@ -0,0 +1,5 @@
+export const GENERATE_COMMAND = ':generate';
+
+export const generate = async (argv: string[]) => {
+  throw new Error('Not implemented');
+};
